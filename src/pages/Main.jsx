a2a0e0f54a5622ef4493e0b567115936e2385c2f
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-19 19:41:12 -0400

    Video background

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import BackgroundVideo from "../assets/charging.mp4";
 
 const Main = () => {
     return (
         <div className="homepage-container">
+            {/* video background */}
+            <video
+                src={BackgroundVideo}
+                className="homepage-container__video"
+                muted
+                autoPlay
+                loop
+            ></video>
+
             <Navbar />
         </div>
     );
