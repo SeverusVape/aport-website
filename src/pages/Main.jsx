4614c181a7338498abba20e8468f3acfe4956696
Veter v Golove <koshelevworkmail@gmail.com>
2022-09-20 16:14:39 -0400

    commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,21 +4,23 @@ import BackgroundVideo from "../assets/charging.mp4";
 
 const Main = () => {
     return (
-        <div className="homepage-container">
-            {/* video background */}
-            <video
-                src={BackgroundVideo}
-                className="homepage-container__video"
-                muted
-                autoPlay
-                loop
-            ></video>
+        <div className="wrapper">
+            <div className="homepage-container">
+                {/* video background */}
+                <video
+                    src={BackgroundVideo}
+                    className="homepage-container__video"
+                    muted
+                    autoPlay
+                    loop
+                ></video>
 
-            <div className="homepage-container__text">
-                Electric Vehicle Charging Service
+                <div className="homepage-container__text">
+                    Electric Vehicle Charging Service
+                </div>
+
+                <Navbar />
             </div>
-
-            <Navbar />
         </div>
     );
 };
